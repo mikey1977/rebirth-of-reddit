b14d0b57101f43ecc0e87dd358963ade3bf2acf8
mikey1977 <mmi@umich.edu>
2015-10-02 21:23:27 -1000

    added link to lists, formatted list size

--- js/app.js
+++ js/app.js
@@ -6,16 +6,31 @@
     url : "https://www.reddit.com/r/The_Donald.json",
     method : "GET",
     success : function(data) {
+
       $('section').append('<ul>');
       for (var i = 0; i < data.data.children.length; i++) {
         var listItems = $('<li>', {
           html : data.data.children[i].data.title
         });
-        $('section > ul').append(listItems);
+
+        var imageItems = ' <a href="'+ data.data.children[i].data.url + '"> ' + 'Click to Dread the Donald'  + '</a>'
+
+        // $('section > ul').append(listItems, imageItems);
+        $(listItems).append(imageItems);
+        $('section > ul').append(
+          $(listItems)
+            // $(imageItems).html()
+            );
+
       };
+
       $('section > ul > li').css({
+        textalign: "center",
+        margin: "20px",
+        padding: "20px",
+        width: "600px",
         border: "1px solid black",
-        height: "40px",
+        height: "100px",
         backgroundColor: "green"
       });
     },
